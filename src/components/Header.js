abd43d009859a08abd028535072bad6de72e856f
CoffeeArt <art.henry007@gmail.com>
2021-06-01 10:28:01 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import ProTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "blue" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.proTypes = {
+  title: ProTypes.string.isRequired,
+};
+
+export default Header;
